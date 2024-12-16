0e2286ffb2244a8307f46ac6098fc6f506cdbeb1
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-12-16 19:39:34 +0400

    added JwtGenerator.cs, BrowserInternalizationProvider.cs, CookieService.cs, LowerInvariantLookupNormalizer.cs services

---
+++ src/Infrastructure/Services/JwtGenerator.cs
@@ -0,0 +1,106 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Application.Common;
+using Application.Services;
+using Domain.Aggregates;
+using Domain.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Services;
+
+public sealed class JwtGenerator : IJwtGenerator
+{
+    public const string CookieName = "authorization";
+
+    public static readonly string ClaimsIssuer = "JWT__ISSUER".FromEnvRequired();
+    public static readonly string ClaimsAudience = "JWT__AUDIENCE".FromEnvRequired();
+    private static readonly string Key = "JWT__KEY".FromEnvRequired();
+
+    public static readonly JwtBearerEvents Events = new()
+    {
+        OnMessageReceived = ctx =>
+        {
+            ctx.Request.Query.TryGetValue(CookieName, out var query);
+            ctx.Request.Headers.TryGetValue(CookieName, out var header);
+            ctx.Request.Cookies.TryGetValue(CookieName, out var cookie);
+            ctx.Token = (string?)query ?? (string?)header ?? cookie;
+            return Task.CompletedTask;
+        },
+        OnForbidden = ctx =>
+        {
+            if (ctx.Request.Path.StartsWithSegments("/api"))
+            {
+                ctx.Response.StatusCode = 403;
+                return Task.CompletedTask;
+            }
+
+            ctx.Response.Redirect($"/404?returnUrl={UrlEncoder.Default.Encode(ctx.Request.Path)}");
+            return Task.CompletedTask;
+        },
+        OnChallenge = ctx =>
+        {
+            if (ctx.Request.Path.StartsWithSegments("/api"))
+            {
+                ctx.Response.StatusCode = 401;
+                return Task.CompletedTask;
+            }
+
+            ctx.Response.Redirect($"/login?returnUrl={UrlEncoder.Default.Encode(ctx.Request.Path)}");
+            ctx.HandleResponse();
+            return Task.CompletedTask;
+        },
+    };
+
+    public static readonly TokenValidationParameters TokenValidationParameters = new()
+    {
+        ValidIssuer = ClaimsIssuer,
+        ValidAudience = ClaimsAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAlgorithms = [SecurityAlgorithms.HmacSha256],
+    };
+
+    private readonly JwtSecurityTokenHandler _handler = new();
+    private readonly SymmetricSecurityKey _securityKey = new(Encoding.UTF8.GetBytes(Key));
+    private SigningCredentials SigningCredentials => new(_securityKey, SecurityAlgorithms.HmacSha256);
+
+    public string GenerateToken(IEnumerable<Claim> claims, TimeSpan? expiration = null)
+    {
+        expiration ??= TimeSpan.FromSeconds(int.Parse("JWT__EXPIRATION".FromEnvRequired()));
+
+        var token = new JwtSecurityToken(
+            ClaimsIssuer,
+            ClaimsAudience,
+            claims,
+            expires: DateTime.UtcNow.Add(expiration.Value),
+            signingCredentials: SigningCredentials);
+
+        return _handler.WriteToken(token);
+    }
+
+    public string GenerateToken(User user, TimeSpan? expiration = null)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimsPrincipalExt.IdClaimType, user.Id.ToString()),
+            new Claim(ClaimsPrincipalExt.PersonalIdClaimType, user.PersonalId),
+            new Claim(ClaimsPrincipalExt.UsernameClaimType, user.Username),
+            new Claim(ClaimsPrincipalExt.EmailClaimType, user.Email),
+            new Claim(ClaimsPrincipalExt.PhoneClaimType, user.PhoneNumber),
+            new Claim(ClaimsPrincipalExt.RoleClaimType, string.Join(';', user.Roles.Select(role => role.RoleId))),
+            new Claim(ClaimsPrincipalExt.SecurityStampClaimType, user.SecurityStamp),
+        };
+        var userClaims = user.Claims.Select(uc => uc.Claim);
+        var roleClaims = user.Roles.SelectMany(ur => ur.Role.Claims.Select(rc => rc.Claim));
+
+        var allClaims = claims.Concat(userClaims).Concat(roleClaims);
+
+        return GenerateToken(allClaims, expiration);
+    }
+}
